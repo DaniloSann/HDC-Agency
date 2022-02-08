d404438a706726b6f97c8919e3b2e90c8aa061c9
Danilo Santana <danilosan262020@gmail.com>
2022-02-08 20:11:40 -0300

    Grid com parallax

--- js/scripts.js
+++ js/scripts.js
@@ -106,6 +106,8 @@ $(document).ready(function(){
 
         $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png'});
 
+        $('#apply-area').parallax({ imageSrc: 'img/pattern.png'});
+
     }, 250);
 
 });
